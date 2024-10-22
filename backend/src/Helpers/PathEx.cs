5d8fd3a6655dd4628eb7a5a0e65657f22af46557
GGG <gggkiller2@gmail.com>
2024-10-21 21:09:29 -0300

    Improve PathEx.

--- backend/src/Helpers/PathEx.cs
+++ backend/src/Helpers/PathEx.cs
@@ -8,13 +8,22 @@ public static partial class PathEx
     [GeneratedRegex(@"[\0-\31<>:""\/\\|?*]+|[\s.]+$")]
     private static partial Regex ForbiddenCharsRegex();
 
+    // This doesn't include .. nor . because it'll be handled by the forbidden chars regex.
     private static readonly FrozenSet<string> s_invalidFileNames = FrozenSet.Create(
         StringComparer.OrdinalIgnoreCase,
         "CON", "PRN", "AUX", "NUL", "COM0", "COM1", "COM2", "COM3", "COM4", "COM5",
         "COM6", "COM7", "COM8", "COM9", "COM¹", "COM²", "COM³", "LPT0", "LPT1", "LPT2",
-        "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9", "LPT¹", "LPT²", "LPT³",
-        "..", ".");
+        "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9", "LPT¹", "LPT²", "LPT³");
 
+    /// <summary>
+    /// Sanitizes file names according to https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file#naming-conventions
+    /// </summary>
+    /// <remarks>
+    /// This function explicitly forces Windows rules upon Windows as well because Windows' rules make more sense to me and should
+    /// cause the least trouble with most tools, CIFS/SMB and other things.
+    /// </remarks>
+    /// <param name="fileName">File name to sanitize.</param>
+    /// <returns>The sanitized file name.</returns>
     public static string SanitizeFileName(string fileName)
     {
         // Remove all unallowed chars from the name.
@@ -30,9 +39,15 @@ public static partial class PathEx
         return name;
     }
 
+    /// <summary>
+    /// <para>Tries to find the full path to a binary.</para>
+    /// <para>If it's a path, we return it if it exists, otherwise we just search for it in all PATH components.</para>
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
     public static string? GetBinaryPath(string name)
     {
-        if (Path.IsPathRooted(name))
+        if (name.Contains('/'))
         {
             return File.Exists(name) ? name : null;
         }
@@ -44,7 +59,10 @@ public static partial class PathEx
         foreach (var basePath in basePaths)
         {
             string path = Path.Combine(basePath, name);
-            if (File.Exists(path))
+            if (File.Exists(path)
+                && (OperatingSystem.IsWindows()
+                    // Need to check if file is executable on unix-like platforms.
+                    || (File.GetUnixFileMode(path) & (UnixFileMode.UserExecute | UnixFileMode.GroupExecute | UnixFileMode.OtherExecute)) != 0))
             {
                 return path;
             }
