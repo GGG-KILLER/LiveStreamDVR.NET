e5a9a4381a118423cbec09bf1886bef058d37929
GGG <gggkiller2@gmail.com>
2024-10-22 01:12:06 -0300

    Split stream capturer into methods for easier readability.

--- backend/src/Services/Capture/TwitchStreamCapturer.cs
+++ backend/src/Services/Capture/TwitchStreamCapturer.cs
@@ -31,7 +31,7 @@ public sealed class TwitchStreamCapturer(
                 _capturesInProgress.RemoveAll(task => task.IsCompleted);
 
                 logger.LogInformation("Starting to capture {Stream}", stream);
-                _capturesInProgress.Add(CaptureAsync(stream, stoppingToken));
+                _capturesInProgress.Add(ProcessCaptureAsync(stream, stoppingToken));
             }
             catch (OperationCanceledException) { }
             catch (Exception ex)
@@ -44,18 +44,17 @@ public sealed class TwitchStreamCapturer(
         logger.LogInformation("TwitchStreamCapturer shut down.");
     }
 
-    private async Task CaptureAsync(TwitchCapture stream, CancellationToken cancellationToken = default)
+    private async Task ProcessCaptureAsync(TwitchCapture stream, CancellationToken cancellationToken = default)
     {
         try
         {
             var binariesOptions = binariesOptionsMonitor.CurrentValue;
-            var captureOptions = captureOptionsMonitor.CurrentValue;
 
             using var _1 = logger.BeginScope("Capture of Stream: {Stream}", stream);
 
             Directory.CreateDirectory("logs");
 
-            var outputDir = Path.Combine(captureOptions.OutputDirectory, PathEx.SanitizeFileName(stream.UserName));
+            var outputDir = Path.Combine(captureOptionsMonitor.CurrentValue.OutputDirectory, PathEx.SanitizeFileName(stream.UserName));
             var outputDirInfo = new DirectoryInfo(outputDir);
             if (!outputDirInfo.Exists)
             {
@@ -71,14 +70,37 @@ public sealed class TwitchStreamCapturer(
             var outputFileTs = Path.Combine(outputDir, PathEx.SanitizeFileName($"{stream.StartedAt:yyyy-MM-dd' 'HH:mm:ss} {stream.UserName} - {stream.Title} [{stream.Id}].ts"));
             var outputFileMp4 = Path.Combine(outputDir, PathEx.SanitizeFileName($"{stream.StartedAt:yyyy-MM-dd' 'HH:mm:ss} {stream.UserName} - {stream.Title} [{stream.Id}].mp4"));
 
+            var captureResult = await CaptureStreamAsync(stream, outputDir, outputFileTs, cancellationToken)
+                .ConfigureAwait(false);
+            if (!captureResult)
+                return;
+
+            await RemuxStreamCaptureAsync(stream, outputDir, outputFileTs, outputFileMp4, cancellationToken)
+                .ConfigureAwait(false);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error capturing stream {Stream}", stream);
+        }
+        finally
+        {
+            captureManager.NotifyCaptureFinished(stream.Id);
+        }
+    }
+
+    private async Task<bool> CaptureStreamAsync(
+        TwitchCapture stream,
+        string workingDirectory,
+        string outputFile,
+        CancellationToken cancellationToken = default)
+    {
+        logger.LogInformation("Capturing stream {Stream}", stream);
+        var streamlink = new Process()
+        {
+            StartInfo = new ProcessStartInfo
             {
-                logger.LogInformation("Capturing stream {Stream}", stream);
-                var streamlink = new Process()
-                {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = binariesOptions.StreamLinkPath,
-                        ArgumentList =
+                FileName = binariesOptionsMonitor.CurrentValue.StreamLinkPath,
+                ArgumentList =
                         {
                             "--hls-live-edge", "99999",
                             "--stream-timeout", "200",
@@ -90,128 +112,127 @@ public sealed class TwitchStreamCapturer(
                             "--twitch-disable-reruns",
                             "--retry-streams", "10",
                             "--retry-max", "5",
-                            "-o", outputFileTs,
+                            "-o", outputFile,
                             "--url", $"https://twitch.tv/{stream.Login}",
                             "--default-stream", "1080p60,best"
                         },
-                        UseShellExecute = false,
-                        RedirectStandardError = true,
-                        RedirectStandardOutput = true,
-                        StandardErrorEncoding = Encoding.UTF8,
-                        StandardOutputEncoding = Encoding.UTF8,
-                        WorkingDirectory = outputDir,
-                    }
-                };
-                var streamlinkExtraFlags = configuration.StreamlinkExtraCommandLine;
-                if (!string.IsNullOrWhiteSpace(streamlinkExtraFlags))
-                {
-                    foreach (var flag in CommandLineSplitter.SplitArguments(streamlinkExtraFlags))
-                        streamlink.StartInfo.ArgumentList.Add(flag);
-                }
-                string streamlinkCommandLine = "$ " + CommandLineSplitter.JoinArguments([streamlink.StartInfo.FileName, .. streamlink.StartInfo.ArgumentList]);
-                logger.LogInformation("Using command line {CommandLine}", streamlinkCommandLine);
-
-                using var streamlinkStdout = new StreamWriter(Path.Combine("logs", $"capture_{stream.Login}_{stream.Id}.stdout.log"));
-                using var streamlinkStderr = new StreamWriter(Path.Combine("logs", $"capture_{stream.Login}_{stream.Id}.stderr.log"));
-                await streamlinkStdout.WriteLineAsync(streamlinkCommandLine);
-                await streamlinkStderr.WriteLineAsync(streamlinkCommandLine);
-
-                var streamlinkEx = new ProcessEx(streamlink);
-                streamlinkEx.OnStandardOutputLine += (_, line) =>
-                {
-                    streamlinkStdout.WriteLine(line);
-                    streamlinkStdout.Flush();
-                };
-                streamlinkEx.OnStandardErrorLine += (_, line) =>
-                {
-                    streamlinkStderr.WriteLine(line);
-                    streamlinkStderr.Flush();
-                };
-                await streamlinkEx.StartAndWaitAsync(cancellationToken).ConfigureAwait(false);
-                if (streamlink.ExitCode != 0)
-                {
-                    logger.LogError("Failed to capture stream {Stream}. Exit code was {ExitCode}.", stream, streamlink.ExitCode);
-                    return;
-                }
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                StandardErrorEncoding = Encoding.UTF8,
+                StandardOutputEncoding = Encoding.UTF8,
+                WorkingDirectory = workingDirectory,
             }
+        };
 
-            {
-                logger.LogInformation("Remuxing stream {Stream}", stream);
-                var ffmpeg = new Process()
-                {
-                    StartInfo =
+        var streamlinkExtraFlags = configuration.StreamlinkExtraCommandLine;
+        if (!string.IsNullOrWhiteSpace(streamlinkExtraFlags))
+        {
+            foreach (var flag in CommandLineSplitter.SplitArguments(streamlinkExtraFlags))
+                streamlink.StartInfo.ArgumentList.Add(flag);
+        }
+
+        string streamlinkCommandLine = "$ " + CommandLineSplitter.JoinArguments([streamlink.StartInfo.FileName, .. streamlink.StartInfo.ArgumentList]);
+        logger.LogInformation("Using command line {CommandLine}", streamlinkCommandLine);
+        var streamlinkStdout = new StreamWriter(Path.Combine("logs", $"capture_{stream.Login}_{stream.Id}.stdout.log"));
+        var streamlinkStderr = new StreamWriter(Path.Combine("logs", $"capture_{stream.Login}_{stream.Id}.stderr.log"));
+        await streamlinkStdout.WriteLineAsync(streamlinkCommandLine);
+        await streamlinkStderr.WriteLineAsync(streamlinkCommandLine);
+
+        var streamlinkEx = new ProcessEx(streamlink);
+        streamlinkEx.OnStandardOutputLine += (_, line) =>
+        {
+            streamlinkStdout.WriteLine(line);
+            streamlinkStdout.Flush();
+        };
+        streamlinkEx.OnStandardErrorLine += (_, line) =>
+        {
+            streamlinkStderr.WriteLine(line);
+            streamlinkStderr.Flush();
+        };
+        await streamlinkEx.StartAndWaitAsync(cancellationToken).ConfigureAwait(false);
+
+        if (streamlink.ExitCode != 0)
+        {
+            logger.LogError("Failed to capture stream {Stream}. Exit code was {ExitCode}.", stream, streamlink.ExitCode);
+            return false;
+        }
+
+        return true;
+    }
+
+    private async Task RemuxStreamCaptureAsync(
+        TwitchCapture stream,
+        string workingDirectory,
+        string inputFile,
+        string outputFile,
+        CancellationToken cancellationToken = default)
+    {
+        logger.LogInformation("Remuxing stream {Stream}", stream);
+        var ffmpeg = new Process()
+        {
+            StartInfo =
                     {
-                        FileName = binariesOptions.FfmpegPath,
+                        FileName = binariesOptionsMonitor.CurrentValue.FfmpegPath,
                         ArgumentList =
                         {
-                            "-i", outputFileTs,
+                            "-i", inputFile,
                             "-c", "copy",
                             "-bsf:a", "aac_adtstoasc",
                             "-movflags", "faststart",
-                            outputFileMp4
+                            outputFile
                         },
                         UseShellExecute = false,
                         RedirectStandardError = true,
                         RedirectStandardOutput = true,
                         StandardErrorEncoding = Encoding.UTF8,
                         StandardOutputEncoding = Encoding.UTF8,
-                        WorkingDirectory = outputDir,
+                        WorkingDirectory = workingDirectory,
                     }
-                };
+        };
 
-                var ffmpegExtraFlags = configuration.FfmpegExtraCommandLine;
-                if (!string.IsNullOrWhiteSpace(ffmpegExtraFlags))
-                {
-                    foreach (var flag in CommandLineSplitter.SplitArguments(ffmpegExtraFlags))
-                        ffmpeg.StartInfo.ArgumentList.Add(flag);
-                }
-
-                string ffmpegCommandLine = "$ " + CommandLineSplitter.JoinArguments([ffmpeg.StartInfo.FileName, .. ffmpeg.StartInfo.ArgumentList]);
-                logger.LogInformation("Using command line {CommandLine}", ffmpegCommandLine);
-
-                using var ffmpegStdout = new StreamWriter(Path.Combine("logs", $"remux_{stream.Login}_{stream.Id}.stdout.log"));
-                using var ffmpegStderr = new StreamWriter(Path.Combine("logs", $"remux_{stream.Login}_{stream.Id}.stderr.log"));
-                await ffmpegStdout.WriteLineAsync(ffmpegCommandLine);
-                await ffmpegStderr.WriteLineAsync(ffmpegCommandLine);
-
-                var ffmpegEx = new ProcessEx(ffmpeg);
-                ffmpegEx.OnStandardOutputLine += (_, line) =>
-                {
-                    ffmpegStdout.WriteLine(line);
-                    ffmpegStdout.Flush();
-                };
-                ffmpegEx.OnStandardErrorLine += (_, line) =>
-                {
-                    ffmpegStderr.WriteLine(line);
-                    ffmpegStderr.Flush();
-                };
-                await ffmpegEx.StartAndWaitAsync(cancellationToken).ConfigureAwait(false);
-
-                if (File.Exists(outputFileMp4) && OperatingSystem.IsLinux())
-                {
-                    File.SetUnixFileMode(
-                        outputFileMp4,
-                        UnixFileMode.UserRead | UnixFileMode.UserWrite
-                        | UnixFileMode.GroupRead | UnixFileMode.GroupWrite
-                        | UnixFileMode.OtherRead);
-                }
-
-                if (ffmpeg.ExitCode != 0)
-                {
-                    logger.LogError("Failed to remux {Stream}. Exit code was {ExitCode}.", stream, ffmpeg.ExitCode);
-                    return;
-                }
-
-                File.Delete(outputFileTs);
-            }
-        }
-        catch (Exception ex)
+        var ffmpegExtraFlags = configuration.FfmpegExtraCommandLine;
+        if (!string.IsNullOrWhiteSpace(ffmpegExtraFlags))
         {
-            logger.LogError(ex, "Error capturing stream {Stream}", stream);
+            foreach (var flag in CommandLineSplitter.SplitArguments(ffmpegExtraFlags))
+                ffmpeg.StartInfo.ArgumentList.Add(flag);
         }
-        finally
+
+        string ffmpegCommandLine = "$ " + CommandLineSplitter.JoinArguments([ffmpeg.StartInfo.FileName, .. ffmpeg.StartInfo.ArgumentList]);
+        logger.LogInformation("Using command line {CommandLine}", ffmpegCommandLine);
+        var ffmpegStdout = new StreamWriter(Path.Combine("logs", $"remux_{stream.Login}_{stream.Id}.stdout.log"));
+        var ffmpegStderr = new StreamWriter(Path.Combine("logs", $"remux_{stream.Login}_{stream.Id}.stderr.log"));
+        await ffmpegStdout.WriteLineAsync(ffmpegCommandLine);
+        await ffmpegStderr.WriteLineAsync(ffmpegCommandLine);
+
+        var ffmpegEx = new ProcessEx(ffmpeg);
+        ffmpegEx.OnStandardOutputLine += (_, line) =>
         {
-            captureManager.NotifyCaptureFinished(stream.Id);
+            ffmpegStdout.WriteLine(line);
+            ffmpegStdout.Flush();
+        };
+        ffmpegEx.OnStandardErrorLine += (_, line) =>
+        {
+            ffmpegStderr.WriteLine(line);
+            ffmpegStderr.Flush();
+        };
+        await ffmpegEx.StartAndWaitAsync(cancellationToken).ConfigureAwait(false);
+
+        if (File.Exists(outputFile) && OperatingSystem.IsLinux())
+        {
+            File.SetUnixFileMode(
+                outputFile,
+                UnixFileMode.UserRead | UnixFileMode.UserWrite
+                | UnixFileMode.GroupRead | UnixFileMode.GroupWrite
+                | UnixFileMode.OtherRead);
         }
+
+        if (ffmpeg.ExitCode != 0)
+        {
+            logger.LogError("Failed to remux {Stream}. Exit code was {ExitCode}.", stream, ffmpeg.ExitCode);
+            return;
+        }
+
+        File.Delete(inputFile);
     }
 }
